2bd48a28aabca415f1429c1fd2ea3947a7b7e22b
Sakshi Singh <1539sakshi@gmail.com>
2023-03-26 20:41:09 +0530

    Created #3: created card component

--- monster_card/src/App.js
+++ monster_card/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBox from './components/SearchBox';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <h1>Monters</h1>
       <SearchBox/>
+      <Card/>
     </div>
   );
 }
---
+++ monster_card/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className='card'>
+      <img src={'https://robohash.org/$19?set=set2&size=180x180'}/>
+      <h1>Monster Name</h1>
+      <p>Monster Email id</p>
+    </div>
+  );
+}
+
+export default Card;
