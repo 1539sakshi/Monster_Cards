b6335b33a64f88b4cea543297fa48a24da32df28
Sakshi Singh <1539sakshi@gmail.com>
2023-03-27 01:11:42 +0530

    Added #6 : added styling

--- monster_card/src/components/Card.js
+++ monster_card/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({monster}) {
   return (
     <div className='card'>
-      <img src={'https://robohash.org/${monster.id}?set=set2&size=180x180'}/>
+      <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
       <h1>{monster.name}</h1>
       <p>{monster.email}</p>
     </div>
