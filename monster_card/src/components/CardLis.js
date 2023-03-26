ee8fe255dc30703d62f9c98888f0b5143003a0e1
Sakshi Singh <1539sakshi@gmail.com>
2023-03-26 22:58:57 +0530

    Created #4: created Card list

---
+++ monster_card/src/components/CardLis.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from "./Card";
+
+function CardLis() {
+  return (
+    <div className='card-list'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  );
+}
+
+export default CardLis;
